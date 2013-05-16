7cecc32592f827c4d8669147ec7cb122a81ba669
Josiel Moreira <josiel1nico@gmail.com>
2013-05-15 23:27:01 -0300

    Corrigindo o primeiro COMMIT

---
+++ src/Magic_Buffet.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package magic_buffet;
+
+/**
+ *
+ * @author Josiel
+ */
+public class Magic_Buffet {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
